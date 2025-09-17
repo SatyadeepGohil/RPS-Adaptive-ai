4340272c91fe4a9499778005d5ee53fba4d0fdc5
SatyadeepGohil <gohilsatyadeep7@gmail.com>
2025-09-17 12:46:16 +0530

    perf(frontend): reduce element render delay
    - Inline CSS from `index.html` and `classic.html` directly in headers
    - Add `<link rel="preconnect">` tags to eliminate network dependency chain
    - Remove linked CSS tags, reducing initial load by ~120–200ms
    - Remove experimental `caculateOccurence` method from `n-grams`
    - Remove `scss` folder and uninstall related dependencies

--- src/logic/n-grams.ts
+++ src/logic/n-grams.ts
@@ -1,4 +1,3 @@
-import { useGameStore } from "../store/GameStore.js";
 import { N_Grams_Type } from "@/types/nGramsType.js";
 
 class N_Grams implements N_Grams_Type {
@@ -14,31 +13,8 @@ class N_Grams implements N_Grams_Type {
         this.sProb = 0;
     }
 
-    calculateOccurencDistribution() {
-        const { attackHistory } = useGameStore.getState();
-
-        if (attackHistory.length === 0) return;
-
-        let rCount = 0, pCount = 0, sCount = 0;
-
-        for (let attack of attackHistory) {
-            switch(attack) {
-                case 'rock':
-                    rCount++;
-                    this.rProb = rCount / attackHistory.length;
-                    break;
-                case 'paper':
-                    pCount++;
-                    this.pProb = pCount / attackHistory.length;
-                    break;
-                case 'scissors':
-                    sCount++;
-                    this.sProb = sCount / attackHistory.length;
-                    break;
-            }
-        }
+    patternRecognition() {
     }
-
 }
 
 export default N_Grams;
